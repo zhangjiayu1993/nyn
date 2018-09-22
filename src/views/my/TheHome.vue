73199bff6c3c74c9b5f78cc7b6ab6cc6bcd8ff68
zhangjiayu1993 <498701604@qq.com>
2018-09-22 23:41:50 +0800

    关于我们

--- src/views/my/TheHome.vue
+++ src/views/my/TheHome.vue
@@ -2,8 +2,30 @@
 <div class="wraper">
   <div class="avatar">
     <img src="./avatar.jpg" class="image"/>
-    <span class="name"></span>
+    <span class="name">周杰伦</span>
   </div>
+  <van-cell-group class="cell">
+    <van-cell is-link>
+      <template slot="title">
+        <i class="iconfont icon-dingdan"></i>
+        <span class="van-cell-text">我的订单</span>
+      </template>
+    </van-cell>
+    <van-cell  is-link>
+      <template slot="title">
+        <i class="iconfont icon-hezuo"></i>
+        <span class="van-cell-text">商务合作</span>
+      </template>
+    </van-cell>
+  </van-cell-group>
+  <van-cell-group class="cell">
+    <van-cell is-link>
+      <template slot="title">
+        <i class="iconfont icon-guanyuwomen"></i>
+        <span class="van-cell-text">关于我们</span>
+      </template>
+    </van-cell>
+  </van-cell-group>
   <TheFooter :selected="selected"></TheFooter>
 </div>
 </template>
@@ -23,20 +45,39 @@ export default {
 
 <style scoped lang="less">
 .wraper{
-  padding-bottom: 55px;
+  min-height: 100%;
+  background-color: #f4f4f4;
   .avatar{
     width: 100%;
-    height: 2rem;
+    height: 1.6rem;
+    padding-top: 0.4rem;
     background: url("./bg.jpg") no-repeat;
     background-size: 100%;
+    margin-bottom: 10px;
     .image{
       display: inline-block;
       vertical-align: middle;
       width: 1.2rem;
       height: 1.2rem;
       border-radius: 50%;
-      margin-top: 0.4rem;
       margin-left: 3%;
+      margin-right: 10px;
+    }
+    .name{
+      display: inline-block;
+      line-height: 1.2rem;
+    }
+  }
+  .cell{
+    margin-bottom: 10px;
+    .icon-dingdan{
+      color: #da8988;
+    }
+    .icon-hezuo{
+      color: #d4ca79;
+    }
+    .icon-guanyuwomen{
+      color: #da8988;
     }
   }
 }
