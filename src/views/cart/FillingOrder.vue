08929bb0caa3969155143acfd40e5e47c98eb9d9
zhangjiayu <498701604@qq.com>
2018-10-05 01:20:15 +0800

    bug fix

--- src/views/cart/FillingOrder.vue
+++ src/views/cart/FillingOrder.vue
@@ -146,6 +146,7 @@ export default {
           let data = JSON.parse(res.data.data)
           console.log(data);
           wx.chooseWXPay({
+            appId: data.appId,
             timestamp: data.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
             nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位
             package: data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
