af1e0bf76b7dcb19b94288ec5e0b0619ae50ba04
zhangjiayu <498701604@qq.com>
2018-10-05 01:52:53 +0800

    bug fix

--- src/views/cart/FillingOrder.vue
+++ src/views/cart/FillingOrder.vue
@@ -144,28 +144,40 @@ export default {
       this.$axios.post(PAY_WECHAT, {token: this.token, order_id: id}).then(res => {
         if (res.data.error_code === 0) {
           let data = JSON.parse(res.data.data)
-          console.log(data);
-          wx.chooseWXPay({
-            appId: data.appId,
-            timestamp: data.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-            nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位
-            package: data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-            signType: data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-            paySign: data.paySign, // 支付签名
-            success: function (res) {
-              // 支付成功后的回调函数
-              if (res.err_msg === 'get_brand_wcpay_request:ok') {
-                this.$router.push({
-                  path: '/paysucess',
-                  name: 'PaySucess'
-                })
-              } else {
-                this.$router.push({
-                  path: '/payfailed',
-                  name: 'PayFailed'
-                })
+          wx.config({
+            debug: true,
+            appId: data.appId, // 必填，公众号的唯一标识
+            timestamp: data.timeStamp, // 必填，生成签名的时间戳
+            nonceStr: data.nonceStr, // 必填，生成签名的随机串
+            signature: data.signature, // 必填，微信签名，这个签名，和下面的paySign,不是同一个。生成方法，见http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html 附录1
+            jsApiList: [
+              'chooseWXPay'
+            ] // 必填，需要使用的JS接口列表
+          });
+          wx.ready(function() {
+            wx.chooseWXPay({
+              appId: data.appId,
+              timestamp: data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+              nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位
+              package: data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+              signType: data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+              paySign: data.paySign, // 支付签名
+              success: function (res) {
+                console.log(res);
+                // 支付成功后的回调函数
+                if (res.err_msg === 'get_brand_wcpay_request:ok') {
+                  this.$router.push({
+                    path: '/paysucess',
+                    name: 'PaySucess'
+                  })
+                } else {
+                  this.$router.push({
+                    path: '/payfailed',
+                    name: 'PayFailed'
+                  })
+                }
               }
-            }
+            })
           });
         }
       })
